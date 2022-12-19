fb0b834faa23712453ba09c3f7bc00c2ea1b383b
Mariia Kovalova <mashuha1698@gmail.com>
2022-12-19 17:44:42 +0200

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = ({ seconds }) => {
+  localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const getSeconds = () => {
+  return localStorage.getItem(STORAGE_KEY);
+};
+
+player
+  .setCurrentTime(getSeconds())
+  .then()
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('[name="email"]'),
+  messageText: document.querySelector('[name="message"]'),
+};
+
+populateInfo();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (localStorage.getItem(STORAGE_KEY)) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
+  e.currentTarget.reset();
+  Object.keys(formData).forEach(key => delete formData[key]);
+}
+
+function populateInfo() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    const { email, message } = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (email) {
+      refs.emailInput.value = email;
+    }
+    if (message) {
+      refs.messageText.value = message;
+    }
+  }
+}
