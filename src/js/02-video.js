be1eef67ca123070d685b7c677926a9736157d2e
Mariia Kovalova <mashuha1698@gmail.com>
2023-01-03 15:24:00 +0200

    player upg

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import localStorageApi from './local-storage-api';
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = ({ seconds }) => {
-  localStorage.setItem(STORAGE_KEY, seconds);
-};
-
 player.on('timeupdate', throttle(onPlay, 1000));
 
-const getSeconds = () => {
-  return localStorage.getItem(STORAGE_KEY) || 0;
-};
+player.setCurrentTime(() => localStorageApi.load(STORAGE_KEY) || 0);
 
-player.setCurrentTime(getSeconds());
+function onPlay({ seconds }) {
+  localStorageApi.save(STORAGE_KEY, seconds);
+}
